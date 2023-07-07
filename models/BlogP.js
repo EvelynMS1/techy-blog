98dca7e4cffa8beadbbb49ecd10b4695d4c02830
EvelynMS1 <102782127+EvelynMS1@users.noreply.github.com>
2023-07-06 19:25:13 -0700

    working on the border outline with thick top border

--- models/BlogP.js
+++ models/BlogP.js
@@ -1,45 +1,44 @@
-const {Model,DataTypes} = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
 
-class BlogP extends Model{
-
-}
+class BlogP extends Model {}
 
 BlogP.init(
-{
-    id:{
-        type:DataTypes.INTEGER,
-        allowNull:false,
-        autoIncrement:true,
-        primaryKey:true
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    title:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    content:{
-        type:DataTypes.TEXT,
-        allowNull:false,
-
+    date: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
     },
-    user_id:{
-        type:DataTypes.INTEGER,
-        references:{
-            model:'user',
-            key:'id'
-        }
-
-
-    }
-},
-{
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
     sequelize,
-    timestamps:true,
-    freezeTableName:true,
-    underscored:true,
-    modelName:'blogp',
-}
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blogp",
+  }
 );
 
 module.exports = BlogP;
